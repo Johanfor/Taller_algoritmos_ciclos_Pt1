d5510bcc2440571c417aad5de8286fb57c08384e
Johan Forero <gohan231985@gmail.com>
2025-08-22 16:07:19 -0500

    Realizo el punto 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,1 +1,11 @@
 //10. Desarrolla una función que genere el total de las tablas de multiplicar dado un numero entero//
+function generarTablasMultiplicar(numero) {
+    let tablas = [];
+    for (let i = 1; i <= 10; i++) {
+        tablas.push(`${numero} x ${i} = ${numero * i}`);
+    }
+    return tablas;
+}   
+let numeroTabla = 5; // Cambia este número para generar la tabla de multiplicar deseada
+let tablasMultiplicar = generarTablasMultiplicar(numeroTabla);
+console.log(`Tabla de multiplicar del ${numeroTabla}:`);
